fe5abfdd56a7efe299650720e656cef93a282e0b
Fredy <frederikhaselmeier@gmail.com>
2017-05-31 12:50:47 +0200

    updated minor version

--- MySQLOO/source/GMModule.cpp
+++ MySQLOO/source/GMModule.cpp
@@ -6,7 +6,7 @@
 #include <iostream>
 #include <fstream>
 #define MYSQLOO_VERSION "9"
-#define MYSQLOO_MINOR_VERSION "3"
+#define MYSQLOO_MINOR_VERSION "4"
 
 GMOD_MODULE_CLOSE() {
 	/* Deletes all the remaining luaobjects when the server changes map
